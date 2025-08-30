9fb0b9add9a472c176188eba0d240b9aff4d97dc
josephhieu <joseph.nguyenhoanghieu@gmail.com>
2025-08-30 11:03:51 +0700

    assertThrows assertDoesNotThrows assertTimeout

--- 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
+++ 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.List;
@@ -18,6 +19,22 @@ class DemoUtilsTest {
     @BeforeEach
     void setupBeforeEach() {
         demoUtils = new DemoUtils();
+    }
+
+    @DisplayName("Timeout")
+    @Test
+    void testTimeout() {
+
+        assertTimeout(Duration.ofSeconds(3), () -> { demoUtils.checkTimeout();}, "Method should execute in 3 seconds");
+    }
+
+    @DisplayName("Throws and Does Not Throw")
+    @Test
+    void testThrowsAndDoesNotThrow() {
+
+        assertThrows(Exception.class, () -> { demoUtils.throwException(-1); }, "Should throw exception");
+
+        assertDoesNotThrow(() -> { demoUtils.throwException(1); }, "Should not throw exception");
     }
 
     @DisplayName("Array Equals")
