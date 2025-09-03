1e824861c5f7fdc82d427462584ceaef40064ca5
josephhieu <joseph.nguyenhoanghieu@gmail.com>
2025-09-03 21:00:16 +0700

    step 3: Refactor the code

--- 2.00-test-driven-development/src/main/java/com/josephHieu/tdd/FizzBuzz.java
+++ 2.00-test-driven-development/src/main/java/com/josephHieu/tdd/FizzBuzz.java
@@ -4,6 +4,24 @@ public class FizzBuzz {
 
     public static String compute(int i) {
 
+        StringBuilder result = new StringBuilder();
+
+        if (i % 3 == 0) {
+            result.append("Fizz");
+        }
+
+        if (i % 5 == 0) {
+            result.append("Buzz");
+        }
+
+        if (result.isEmpty()) {
+            result.append(i);
+        }
+        return result.toString();
+    }
+
+    /*public static String compute(int i) {
+
         if (i % 5 == 0 && i % 3 == 0) {
             return "FizzBuzz";
         } else if (i % 3 == 0) {
@@ -13,5 +31,5 @@ public class FizzBuzz {
         } else {
             return Integer.toString(i);
         }
-    }
+    }*/
 }
