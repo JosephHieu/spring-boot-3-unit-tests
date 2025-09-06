a6a9600097a5473976f42a181b5ca354d21857b7
josephhieu <joseph.nguyenhoanghieu@gmail.com>
2025-09-06 16:48:47 +0700

    Throwing Exceptions with Mocks

--- 5.00-mocking-mockito/src/test/java/com/luv2code/test/MockAnnotationTest.java
+++ 5.00-mocking-mockito/src/test/java/com/luv2code/test/MockAnnotationTest.java
@@ -13,8 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest(classes= MvcTestingExampleApplication.class)
@@ -78,5 +77,38 @@ public class MockAnnotationTest {
 
         assertNotNull(applicationService.checkNull(studentOne.getStudentGrades()
                 .getMathGradeResults()), "Object should not be null");
+    }
+
+    @DisplayName("Thrown runtime error")
+    @Test
+    public void thrownRuntimeError() {
+        CollegeStudent nullStudent = (CollegeStudent) context.getBean("collegeStudent");
+
+        doThrow(new RuntimeException()).when(applicationDao).checkNull(nullStudent);
+
+        assertThrows(RuntimeException.class, () -> {
+            applicationService.checkNull(nullStudent);
+        });
+
+        verify(applicationDao, times(1)).checkNull(nullStudent);
+    }
+
+    @DisplayName("Multiple Stubbing")
+    @Test
+    public void stubbingConsecutiveCalls() {
+        CollegeStudent nullStudent = (CollegeStudent) context.getBean("collegeStudent");
+
+        when(applicationDao.checkNull(nullStudent))
+                .thenThrow(new RuntimeException())
+                .thenReturn("Do not throw exception second time");
+
+        assertThrows(RuntimeException.class, () -> {
+            applicationService.checkNull(nullStudent);
+        });
+
+        assertEquals("Do not throw exception second time",
+                applicationService.checkNull(nullStudent));
+
+        verify(applicationDao, times(2)).checkNull(nullStudent);
     }
 }
