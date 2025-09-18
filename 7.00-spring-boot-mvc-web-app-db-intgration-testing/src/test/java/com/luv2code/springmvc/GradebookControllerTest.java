4b61d1b5a6cf56c52f4049b2806cec0c29446af8
josephhieu <joseph.nguyenhoanghieu@gmail.com>
2025-09-18 21:21:21 +0700

    MVC Tests for Delete Grade - Invalid Grade ID

--- 7.00-spring-boot-mvc-web-app-db-intgration-testing/src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
+++ 7.00-spring-boot-mvc-web-app-db-intgration-testing/src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
@@ -279,6 +279,22 @@ public class GradebookControllerTest {
         assertFalse(mathGrade.isPresent());
     }
 
+    @Test
+    public void deleteAValidGradeHttpRequestStudentIdDoesNotExistEmptyResponse() throws Exception {
+
+        Optional<MathGrade> mathGrade = mathGradesDao.findById(2);
+
+        assertFalse(mathGrade.isPresent());
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders
+                .get("/grades/{id}/{gradeType}", 2, "math"))
+                .andExpect(status().isOk()).andReturn();
+
+        ModelAndView mav = mvcResult.getModelAndView();
+
+        ModelAndViewAssert.assertViewName(mav, "error");
+    }
+
     @AfterEach
     public void setupAfterTransaction() {
         jdbc.execute(sqlDeleteStudent);
