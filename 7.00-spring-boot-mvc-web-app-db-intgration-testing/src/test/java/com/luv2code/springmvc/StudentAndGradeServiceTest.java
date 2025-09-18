f05ef926d66cb63339b9a344ac073855cb005630
josephhieu <joseph.nguyenhoanghieu@gmail.com>
2025-09-18 19:28:53 +0700

    SQL Scripts in props file Student Service Test

--- 7.00-spring-boot-mvc-web-app-db-intgration-testing/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
+++ 7.00-spring-boot-mvc-web-app-db-intgration-testing/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.TestPropertySource;
@@ -43,6 +44,30 @@ public class StudentAndGradeServiceTest {
 
     @Autowired
     private HistoryGradeDao historyGradeDao;
+
+    @Value("${sql.script.create.student}")
+    private String sqlAddStudent;
+
+    @Value("${sql.script.create.math.grade}")
+    private String sqlAddMathGrade;
+
+    @Value("${sql.script.create.science.grade}")
+    private String sqlAddScienceGrade;
+
+    @Value("${sql.script.create.history.grade}")
+    private String sqlAddHistoryGrade;
+
+    @Value("${sql.script.delete.student}")
+    private String sqlDeleteStudent;
+
+    @Value("${sql.script.delete.math.grade}")
+    private String sqlDeleteMathGrade;
+
+    @Value("${sql.script.delete.science.grade}")
+    private String sqlDeleteScienceGrade;
+
+    @Value("${sql.script.delete.history.grade}")
+    private String sqlDeleteHistoryGrade;
 
     @BeforeEach
     public void setupDatabase() {
